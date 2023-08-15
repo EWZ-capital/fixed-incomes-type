e0e1bc3d4ea1b7a25191b86396859919522020d2
CauaMatheus <caua10000@gmail.com>
2023-08-15 09:03:38 -0300

    docs: adding fixed incomes types

---
+++ index.d.ts
@@ -0,0 +1,76 @@
+export type ProductName =
+  | "LCI"
+  | "LCA"
+  | "LC"
+  | "CDB"
+  | "LIG"
+  | "DEB"
+  | "CRI"
+  | "CRA"
+  | "CFF"
+  | "LFSN"
+  | "LFSC"
+  | "LFG"
+  | "LTEL"
+  | "LF"
+  | "Poupanca"
+  | "COE"
+  | "LFT"
+  | "LTN"
+  | "NTN-F"
+  | "NTN-B"
+  | "NTN-B Principal"
+  | "Tesouro Renda+";
+
+export type IndexType = "Pré-fixado" | "Pós-fixado" | "Híbrido" | "COE";
+
+export type MicroClassType =
+  | "Poupança"
+  | "Pré-fixado"
+  | "Juros"
+  | "Inflação"
+  | "COE"
+  | "Indefinido";
+
+export type IssuerType =
+  | "Emissão Bancária"
+  | "Crédito Privado"
+  | "Tesouro Direto"
+  | "Indefinido";
+
+export type OriginType = "B3" | "Client" | "B3_Client";
+
+export type StrategyType = "Rentabilidade" | "Caixa" | "Reserva";
+
+export type BenchmarkType = "Pré-fixado" | "CDI" | "Selic" | "IPCA" | "IGPM" | "COE";
+
+export interface IFixedIncomes {
+  id: string;
+  tickerSymbol: string;
+  productName: ProductName;
+  corporationName: string;
+  participantDocumentNumber: string
+  issueDate?: Date
+  expirationDate: Date
+  fixedIncomeQuantity: string
+  initialUnitValue: string
+  indexType: IndexType
+  microClass: MicroClassType
+  issuerType: IssuerType
+  origin: OriginType
+  strategy: StrategyType
+  rate: string
+  nickname: string
+  taxFree: boolean
+  benchmark: BenchmarkType
+  initialValue: string
+  grossValue: string
+  netValue: string
+  anualProfit?: string
+  totalProfit: string
+  totalNetProfit: string
+  assetIndex?: string
+  anualNetProfit?: string
+  updated_at: Date
+  created_at: Date;
+}
