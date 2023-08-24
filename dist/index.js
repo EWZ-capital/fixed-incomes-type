4eb8592b7529a7bd412966794a87b41c02331e50
CauaMatheus <caua10000@gmail.com>
2023-08-24 11:47:58 -0300

    feat: making a typescript library

---
+++ dist/index.js
@@ -0,0 +1,73 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.FreeTaxInputValues = exports.BenchmarkTypeValues = exports.StrategyTypeValues = exports.OriginTypeValues = exports.IssuerTypeValues = exports.MicroClassTypeValues = exports.IndexTypeValues = exports.ProductNameValues = void 0;
+exports.ProductNameValues = [
+    "LCI",
+    "LCA",
+    "LC",
+    "CDB",
+    "LIG",
+    "DEB",
+    "CRI",
+    "CRA",
+    "CFF",
+    "LFSN",
+    "LFSC",
+    "LFG",
+    "LTEL",
+    "LF",
+    "Poupança",
+    "COE",
+    "LFT",
+    "LTN",
+    "NTN-F",
+    "NTN-B",
+    "NTN-B Principal",
+    "Tesouro Renda+"
+];
+exports.IndexTypeValues = [
+    "Pré-fixado",
+    "Pós-fixado",
+    "Híbrido",
+    "COE",
+    "CFF"
+];
+exports.MicroClassTypeValues = [
+    "Poupança",
+    "Pré-fixado",
+    "Juros",
+    "Inflação",
+    "COE",
+    "CFF",
+    "Indefinido"
+];
+exports.IssuerTypeValues = [
+    "Emissão Bancária",
+    "Crédito Privado",
+    "Tesouro Direto",
+    "Indefinido"
+];
+exports.OriginTypeValues = [
+    "B3",
+    "Client",
+    "B3_Client"
+];
+exports.StrategyTypeValues = [
+    "Rentabilidade",
+    "Caixa",
+    "Reserva"
+];
+exports.BenchmarkTypeValues = [
+    "Pré-fixado",
+    "CDI",
+    "Selic",
+    "IPCA",
+    "IGPM",
+    "COE",
+    "CFF"
+];
+exports.FreeTaxInputValues = [
+    "Sim",
+    "Não",
+    "Não sei"
+];
